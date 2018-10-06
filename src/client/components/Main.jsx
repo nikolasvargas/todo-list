a79cc61de25464f45170fc6c47469a7ea91a6ba7
Nikolas Vargas <vargasnikolass@gmail.com>
2018-10-06 04:10:19 -0300

    (#7) created Menu component

--- src/client/components/Main.jsx
+++ src/client/components/Main.jsx
@@ -2,9 +2,12 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css';
 import 'modules/font-awesome/css/font-awesome.min.css';
 
 import React from 'react';
+import Menu from '../views/Menu'
+import Routes from '../helpers/routes';
 
 export default props => (
   <div className='container'>
-    <h1>teste</h1>
+    <Menu />
+    <Routes />
   </div>
 )
