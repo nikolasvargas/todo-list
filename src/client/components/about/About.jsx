87b65bdf58f75be56224bbbe4fcfa64397a14a51
Nikolas Vargas <vargasnikolass@gmail.com>
2018-10-06 04:12:43 -0300

    (#11) TODO Main and About Component

---
+++ src/client/components/about/About.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PageHeader from '../../views/PageHeader';
+
+export default props => (
+  <div>
+    <PageHeader name='About' small='me'/>
+  </div>
+)
---
+++ src/client/components/todo/Todo.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PageHeader from '../../views/PageHeader';
+
+class Todo extends Component {
+  render() {
+    return (
+      <div>
+        <PageHeader name='Todo' small='Register' />
+      </div>
+    )
+  }
+}
+
+export default Todo;
