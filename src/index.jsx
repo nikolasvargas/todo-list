a0d33146c37796c73c93239bfed832195247d7bb
Nikolas Vargas <vargasnikolass@gmail.com>
2018-10-06 01:52:16 -0300

    (#1 #2, #4, #5) Webpack configuration, folders restructured and created initial react component

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './client/components/Main';
+
+ReactDOM.render(<App />, document.querySelector('#app'));
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const webpack = require('webpack');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'app.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          miniCssExtractPlugin.loader,
+          "css-loader"
+        ]
+      },
+      {
+        test: /\.(woff|woff2|ttf|eot|svg*.*)$/,
+        use: {
+          loader: 'file-loader',
+          options: {}
+        }
+      }
+    ]
+  },
+  plugins: [
+    new miniCssExtractPlugin({ filename: "app.css" })
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      modules: path.resolve(__dirname, 'node_modules'),
+    }
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    host: 'localhost',
+    port: 3333
+  }
+}
