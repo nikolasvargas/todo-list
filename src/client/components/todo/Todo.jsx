38f7225af256fdfb9b95745e81c42f327b9ce436
Nikolas Vargas <vargasnikolass@gmail.com>
2018-10-07 00:43:04 -0300

    (#17) TODO List have actions now

--- src/client/components/todo/Todo.jsx
+++ src/client/components/todo/Todo.jsx
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import PageHeader from '../../views/PageHeader';
+import axios from 'axios';
+
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
+
+const URL = 'http://localhost:5050/api/todos';
 
 class Todo extends Component {
   constructor(props) {
     super(props);
     this.state =  { description: '', list: [] };
+
     this.handleAdd = this.handleAdd.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleRemove = this.handleRemove.bind(this);
+
+    this.refresh();
   }
 
   handleChange(e) {
@@ -16,8 +24,19 @@ class Todo extends Component {
   }
 
   handleAdd() {
-    console.log('handleAdd called');
-    console.log(this.state);
+    const description = this.state.description;
+    axios.post(URL, { description })
+      .then(_ => this.refresh());
+  }
+
+  handleRemove(todoTask) {
+    axios.delete(`${URL}/${todoTask._id}`)
+      .then(_ => this.refresh())
+  }
+
+  refresh() {
+    axios.get(`${URL}?sort=-createdAt`)
+      .then(res => this.setState({...this.state, description: '', list: res.data}));
   }
 
   render() {
@@ -27,7 +46,8 @@ class Todo extends Component {
         <TodoForm description={this.state.description}
                   handleAdd={this.handleAdd}
                   handleChange={this.handleChange} />
-        <TodoList />
+        <TodoList list={this.state.list}
+                  handleRemove={this.handleRemove} />
       </div>
     )
   }
--- src/client/components/todo/TodoList.jsx
+++ src/client/components/todo/TodoList.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import ButtonAndIcon from '../../utils/ButtonAndIcon';
 
-export default props => (
-  <div>
-    <h1>List</h1>
-  </div>
-)
+export default props => {
+  const renderRows = () => {
+    const list = props.list || []
+    return list.map(todoTask => (
+        <tr key={todoTask._id}>
+          <td>{todoTask.description}</td>
+          <td>
+            <ButtonAndIcon style='danger' icon='trash-o'
+              onClick={() => props.handleRemove(todoTask)} />
+          </td>
+        </tr>
+      )
+    )
+  }
+
+  return (
+    <table className='table'>
+      <thead>
+        <tr>
+          <th>Description</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {renderRows()}
+      </tbody>
+    </table>
+  )
+};
