dd0d859d5fb7dc75b6d9bed58fde7a7149d116ee
Nikolas Vargas <vargasnikolass@gmail.com>
2018-10-08 00:03:35 -0300

    enter key pressed can add a new row at TODO List

--- src/client/components/todo/TodoForm.jsx
+++ src/client/components/todo/TodoForm.jsx
@@ -2,19 +2,28 @@ import React from 'react';
 import Grid from '../../utils/Grid';
 import BtnAndIcon from '../../utils/ButtonAndIcon';
 
-export default props => (
-  <div role='form' className='todoForm'>
-    <Grid cols='12 9 10'>
-      <input id="description"
-             className='form-control'
-             placeholder='Create your task'
-             value={props.description}
-             onChange={props.handleChange} />
-    </Grid>
-    <Grid cols='12 3 2'>
-      <BtnAndIcon style='primary' icon='plus' disabled={props.disabled} onClick={props.handleAdd} />
-      <BtnAndIcon style='info' icon='search' disabled={props.disabled} onClick={props.handleSearch} />
-      <BtnAndIcon style='dark' icon='refresh' onClick={props.handleClearSearch} />
-    </Grid>
-  </div>
-)
+export default props => {
+  const keyHandler = (e) => {
+    if(e.key === 'Enter') {
+      e.shiftKey ? props.handleSearch() : props.handleAdd();
+    }
+  }
+
+  return (
+    <div role='form' className='todoForm'>
+      <Grid cols='12 9 10'>
+        <input id="description"
+              className='form-control'
+              placeholder='Create your task'
+              onKeyUp={keyHandler}
+              value={props.description}
+              onChange={props.handleChange} />
+      </Grid>
+      <Grid cols='12 3 2'>
+        <BtnAndIcon style='primary' icon='plus' disabled={props.disabled} onClick={props.handleAdd} />
+        <BtnAndIcon style='info' icon='search' disabled={props.disabled} onClick={props.handleSearch} />
+        <BtnAndIcon style='dark' icon='refresh' onClick={props.handleClearSearch} />
+      </Grid>
+    </div>
+  )
+}
